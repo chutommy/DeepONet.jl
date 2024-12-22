4e036e9b1214eed91b97eddd03cc8f4e41b78af8
chuto <chuto@merck.com>
2024-12-22 17:00:24 +0100

    Refactor darcy's example

--- examples/example.jl
+++ examples/example.jl
@@ -408,40 +408,46 @@ end
 
 using NPZ
 
-darcy = npzread("data/npz/darcys_flow.npz")
+darcys = npzread("data/npz/darcys_flow.npz")
 
-X_train = darcy["X_train"]
-y_train = darcy["y_train"]
+X_train, y_train = darcys["x_train"], darcys["y_train"]
+X_test, y_test = darcys["x_test"], darcys["y_test"]
 
 X_train = PermutedDimsArray(X_train, (2, 3, 1))
 y_train = PermutedDimsArray(y_train, (2, 3, 1))
-a, b, N = size(y_train)
-a != b && error("a != b")
-M = a
+X_test = PermutedDimsArray(X_test, (2, 3, 1))
+y_test = PermutedDimsArray(y_test, (2, 3, 1))
 
 heatmap(X_train[:, :, 1])
 heatmap(y_train[:, :, 1])
 
-X = zeros(Float32, (M * M, N * M * M))
-Y = zeros(Float32, (1, N * M * M))
-g = zeros(Float32, (2, N * M * M))
+X = cat(X_train, X_test, dims = 3)
+y = cat(y_train, y_test, dims = 3)
+M, M2, N = size(X)
+@assert M == M2
 
+Us = zeros(Float32, (M * M, N * M * M))
+Xs = zeros(Float32, (2, N * M * M))
+Ss = zeros(Float32, (1, N * M * M))
 matrix_embd = [i for i in 1:M, j in 1:M]
-
 for n in 1:N
 	ni = (n - 1) * M * M
 	a = ni + 1
 	b = ni + M * M
 
-	X[:, a:b] .= X_train[:, :, n][:]
-	Y[:, a:b] = y_train[:, :, n][:]
-
-	g[1, a:b] = matrix_embd[:]
-	g[2, a:b] = matrix_embd'[:]
+	Us[:, a:b] .= X[:, :, n][:]
+	Ss[:, a:b] = y[:, :, n][:]
+	Xs[1, a:b] = matrix_embd[:]
+	Xs[2, a:b] = matrix_embd'[:]
 end
 
+uxs_train, uxs_test = uxs_split(Us, Xs, Ss; split = 0.5, toshuffle = true)
+U_train, x_train, S_train = uxs_train
+U_test, x_test, S_test = uxs_test
+
 BATCH_SIZE = 1024
-train_loader = Flux.DataLoader((X, g, Y), batchsize = BATCH_SIZE, shuffle = false);
+train_loader = Flux.DataLoader((U_train, x_train, S_train), batchsize = BATCH_SIZE);
+test_loader = Flux.DataLoader((U_test, x_test, S_test), batchsize = BATCH_SIZE);
 
 model = DeepONet(M * M, 2, 32, [gelu, tanh],
 	branch_sizes = [ntuple(Returns(32), 5)...],
@@ -449,23 +455,10 @@ model = DeepONet(M * M, 2, 32, [gelu, tanh],
 	output_sizes = [1],
 )
 opt_state = Flux.setup(Flux.AdamW(0.0003), model)
+train_losses, test_losses = train!(model, train_loader, test_loader)
+plot(train_losses; yaxis = "loss", label = "train", lw = 2)
+plot!(test_losses; yaxis = "loss", label = "test", lw = 2)
 
-train_losses = []
-@showprogress for epoch in 1:EPOCHS
-	train_loss = 0
-	for (u, pts, s) in train_loader
-		loss, grads = Flux.withgradient(model) do m
-			s_hat = m(u, pts)
-			Flux.Losses.mse(s_hat, s)
-		end
-		Flux.update!(opt_state, model, grads[1])
-		train_loss += loss
-	end
-	push!(train_losses, train_loss / BATCH_SIZE)
-end
-
-
-plot(train_losses; yaxis = "loss", label = "train")
 
 i = 0
 preds = []
